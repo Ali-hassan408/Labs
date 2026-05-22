597ac33cecff34ddd611f8883f3d17c04e401355
Ali <alihassan786.ku@gmail.com>
2026-05-22 19:56:59 +0500

    Lab 11

--- Lab 11/Task 6/Main.java
+++ Lab 11/Task 6/Main.java
@@ -1,6 +1,35 @@
+import java.io.*;
+
 public class Main{
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args)throws Exception{
+
+        FileReader fr=new FileReader("Data.txt");
+        BufferedReader br=new BufferedReader(fr);
+
+        String line;
+        String[] arr;
+
+        int count=0;
+        int lineNum=0;
+        boolean isFound=false;
+
+        while ((line=br.readLine())!=null&&!isFound) { 
+            count=0;
+            arr=line.split(" ");
+
+            for(String val:arr){
+                isFound=val.equals("Java");
+                if(!isFound){
+                    count++;
+                }
+            }
+            if(!isFound)
+                lineNum++;
+        }
+
+        System.out.println("Found at "+lineNum+" "+count);
+
+
     }
 }
